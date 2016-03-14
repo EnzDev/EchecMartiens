42541239a02a2ce21c1fc323ac564359fb51b1f9
Rikosontcuit <happyvivus@gmail.com>
2016-03-14 17:08:59 +0100

    get deplacement

--- src/MoyenPion.java
+++ src/MoyenPion.java
@@ -15,18 +15,40 @@ methode qui liste les déplacements autorisés
 */
 public Liste getDeplacement(int coordDepardX,int coordArriveeX, int coordDepardY, int coordArriveeY){
   Liste deplacement = new Liste();
+  //test d'autorisation de deplacement: inferieur a 2 cas, deplacement vertical, deplacement honrizontal ou deplacement diagonal
   if ((Math.abs(coordDepardX-coordArriveeX))<=2 && (Math.abs(coordDepardY-coordArriveeY)<=2) && ((coordDepardX==coordArriveeX)||(coordDepardY==coordArriveeY)||Math.abs(coordArriveeX-coordDepardX)==Math.abs(coordArriveeY-coordDepardY)){
-    Coordonnee depard = new Coordonnee (coordDepardX,coordDepardY)
+    //deplacement vertical
     if (coordDepardX==coordArriveeX){
-    }(coordDepardY==coordArriveeY)
-    Math.abs(coordArriveeX-coordDepardX)==Math.abs(coordArriveeY-coordDepardY)
+      Coordonnee etape = new Coordonnee(coordArriveeX,coordArriveeY)
+      //pour i de 1 a la taille du déplacement
+        for (int i = 1; i<= Math.abs(coordDepardY-coordArriveeY); i++){
+          etape.setCo(coordArriveeX,coordArriveeY+ i);
+          deplacement.add(etape);
+        }
+      }
+    else{
+      //deplacement honrizontal
+      if(coordDepardY==coordArriveeY){
+        Coordonnee etape = new Coordonnee(coordArriveeX,coordArriveeY)
+        //pour i de 1 a la taille du déplacement
+          for (int i = 1; i<= Math.abs(coordDepardX-coordArriveeX); i++){
+            etape.setCo(coordArriveeX +i ,coordArriveeY);
+            deplacement.add(etape);
+          }
+        }
+      }
+      //deplacement diagonal
+      else{
+        Coordonnee etape = new Coordonnee(coordArriveeX,coordArriveeY)
+        //pour i de 1 a la taille du déplacement
+          for (int i = 1; i<= Math.abs(coordDepardX-coordArriveeX); i++){
+            etape.setCo(coordArriveeX +i ,coordArriveeY);
+            deplacement.add(etape);
+          }
+        }
+      }
 
 
-    Coordonnee arrivee = new Coordonnee (coordArriveeX,coordArriveeY)
-    deplacement.add(depard);
-    deplacement.add(arrivee);
-    return deplacement;
-  }
   else {
     deplacement.add(null);
     }
