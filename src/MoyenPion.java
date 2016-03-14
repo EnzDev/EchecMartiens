1a3ea560932af71be2e288ea07c1731d0826d7a4
Rikosontcuit <happyvivus@gmail.com>
2016-03-14 17:24:53 +0100

    <3

--- src/MoyenPion.java
+++ src/MoyenPion.java
@@ -1,3 +1,4 @@
+import liste.Liste;
 public abstract class MoyenPion{
 /**
 @return score qui donne le nombre de point associé au pion
@@ -16,10 +17,10 @@ methode qui liste les déplacements autorisés
 public Liste getDeplacement(int coordDepardX,int coordArriveeX, int coordDepardY, int coordArriveeY){
   Liste deplacement = new Liste();
   //test d'autorisation de deplacement: inferieur a 2 cas, deplacement vertical, deplacement honrizontal ou deplacement diagonal
-  if ((Math.abs(coordDepardX-coordArriveeX))<=2 && (Math.abs(coordDepardY-coordArriveeY)<=2) && ((coordDepardX==coordArriveeX)||(coordDepardY==coordArriveeY)||Math.abs(coordArriveeX-coordDepardX)==Math.abs(coordArriveeY-coordDepardY)){
+  if ((Math.abs(coordDepardX-coordArriveeX))<=2 && (Math.abs(coordDepardY-coordArriveeY)<=2) && ((coordDepardX==coordArriveeX)||(coordDepardY==coordArriveeY)||Math.abs(coordArriveeX-coordDepardX)==Math.abs(coordArriveeY-coordDepardY))){
     //deplacement vertical
     if (coordDepardX==coordArriveeX){
-      Coordonnee etape = new Coordonnee(coordArriveeX,coordArriveeY)
+      Coordonnee etape = new Coordonnee(coordArriveeX,coordArriveeY);
       //pour i de 1 a la taille du déplacement
         for (int i = 1; i<= Math.abs(coordDepardY-coordArriveeY); i++){
           etape.setCo(coordArriveeX,coordArriveeY+ i);
@@ -29,17 +30,16 @@ public Liste getDeplacement(int coordDepardX,int coordArriveeX, int coordDepardY
     else{
       //deplacement honrizontal
       if(coordDepardY==coordArriveeY){
-        Coordonnee etape = new Coordonnee(coordArriveeX,coordArriveeY)
+        Coordonnee etape = new Coordonnee(coordArriveeX,coordArriveeY);
         //pour i de 1 a la taille du déplacement
           for (int i = 1; i<= Math.abs(coordDepardX-coordArriveeX); i++){
             etape.setCo(coordArriveeX +i ,coordArriveeY);
             deplacement.add(etape);
           }
         }
-      }
       //deplacement diagonal
       else{
-        Coordonnee etape = new Coordonnee(coordArriveeX,coordArriveeY)
+        Coordonnee etape = new Coordonnee(coordArriveeX,coordArriveeY);
         //pour i de 1 a la taille du déplacement
           for (int i = 1; i<= Math.abs(coordDepardX-coordArriveeX); i++){
             etape.setCo(coordArriveeX +i ,coordArriveeY);
@@ -47,11 +47,17 @@ public Liste getDeplacement(int coordDepardX,int coordArriveeX, int coordDepardY
           }
         }
       }
+    }
+  // unauthorized move
   else {
     deplacement.add(null);
     }
 }
+
+
 public String toString() {
   return "valeur: " + this.getScore();
 }
+
+
 }
