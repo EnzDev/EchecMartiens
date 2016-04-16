245cb547ac5e2aa0992c8810b789ebcda99b540d
EnzDev <enzo@mallard.ml>
2016-04-16 10:03:14 +0200

    Fixed LinuxBuild.sh

--- LinuxBuild.sh
+++ LinuxBuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-rmdir -rf out 2> /dev/null
+rm -rf out 2> /dev/null
 mkdir out
 
 echo "Compilation du package clavier"
@@ -10,15 +10,15 @@ echo -e "\n"
 echo "Compilation du package liste"
 javac src/liste/* -d out/
 
-echo -e \n
+echo -e "\n"
 echo "Compilation du jeu (encodage UTF-8 pour l'affichage du plateau)"
 javac src/jeu/* -d out/ -cp out/ -encoding UTF8
 javac src/testJeu.java -d out/ -cp out/
 
-echo -e \n
+echo -e "\n"
 echo "Lancer le jeu avec :"
 echo "\$ cd out"
 echo "\$ java testJeu"
 
-echo -e \n
+echo -e "\n"
 echo "Fin de la compilation..."
